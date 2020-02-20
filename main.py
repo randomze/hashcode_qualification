38d4412f2f4d914e9fb209c2046ab501a04223b8
José Pedro <zemiguel.apedro@gmail.com>
2020-02-20 19:09:54 +0000

    merda 2

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ def parse_file(input_file):
 
     b, l, d = [int(x) for x in lines[0].split()]
 
-    book_scores = [int(x) for x in lines[1].split()]
+    book_scores = [[int(x), int(x), -1] for x in lines[1].split()]
     libraries = []
     for i in range(2, 2 * (l+1), 2):
-        print(i)
         n, t, m = [int(x) for x in lines[i].split()]
-        book_ids = [int(x) for x in lines[i + 1].split()]
+        book_ids = set([int(x) for x in lines[i + 1].split()])
+        average = sum(book_ids) / n
+        to_be_removed = set()
+        for j in book_ids:
+            if book_scores[j][2] == -1:
+                book_scores[j][2] = (i - 2) // 2
+                book_scores[j][1] = book_scores[j][0] - average
+            elif book_scores[j][1] < book_scores[j][0] - average:
+                libraries[book_scores[j][2]][1].remove(j)
+                libraries[book_scores[j][2]][2] = sum(libraries[book_scores[j][2]][0])
+                book_scores[j][2] = (i - 2) // 2
+                book_scores[j][1] = book_scores[j][0] - average
+            else:
+                to_be_removed.add(j)
+
+        book_ids = book_ids.difference(to_be_removed)
         libraries += [((n, t, m), book_ids, sum(book_ids))]
 
     return ((b, l, d), book_scores, libraries) 
